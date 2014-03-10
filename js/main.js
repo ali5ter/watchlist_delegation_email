f5443b6fea00a2d6a422678697e08c5686da701b
Alister Lewis-Bowen <alister@different.com>
2014-03-10 10:09:51 -0400

    backout promises until better browser converage

--- js/main.js
+++ js/main.js
@@ -11,37 +11,34 @@
  * @function createMockups
  * Fill out the child nodes of the section element containing the mobile
  * device mockup.
- * @return Promise resolved once all mockup sections filled out
  *
  */
 
-function createMockups() {
-    return new Promise(function(resolve, reject) {
+function createMockups(onComplete) {
 
-        'use strict';
+    'use strict';
 
-        var titleText, contentFile,
-            titleElem, linkElem, containerElem,
-            mockups = $('.mockup'),
-            mockupLen = mockups.length -1;
+    var titleText, contentFile,
+        titleElem, linkElem, containerElem,
+        mockups = $('.mockup'),
+        mockupLen = mockups.length -1;
 
-        mockups.each(function(i) {
-            titleText = this.dataset.title;
-            contentFile = this.dataset.file +'.html';
+    mockups.each(function(i) {
+        titleText = this.dataset.title;
+        contentFile = this.dataset.file +'.html';
 
-            titleElem = $('<h3>', {text: titleText +' '}).append(
-                $('<a>', {href: contentFile, alt: 'content without the chrome', text: 'content'})
-            );
-            containerElem = $('<div>', {class: 'container'}).append(
-                $('<img>', {src:'imgs/container.png', alt: 'device chrome and mail app heading'}),
-                $('<div>', {class: 'content', 'data-file': this.dataset.file})
-            );
+        titleElem = $('<h3>', {text: titleText +' '}).append(
+            $('<a>', {href: contentFile, alt: 'content without the chrome', text: 'content'})
+        );
+        containerElem = $('<div>', {class: 'container'}).append(
+            $('<img>', {src:'imgs/container.png', alt: 'device chrome and mail app heading'}),
+            $('<div>', {class: 'content', 'data-file': this.dataset.file})
+        );
 
-            $(this).append(titleElem, containerElem);
-            console.debug('created mockup layout for '+ this.dataset.file);
+        $(this).append(titleElem, containerElem);
+        console.debug('created mockup layout for '+ this.dataset.file);
 
-            if (mockupLen === i) return resolve();
-        });
+        if (mockupLen ===i && onComplete) return onComplete();
     });
 }
 
@@ -50,54 +47,48 @@ function createMockups() {
  * @function styleMockups
  * Apply style to all mockup container and content class elements declared in
  * the configuration data structure
- * @return Promise resolved once all definitions are applied
  *
  */
 
-function styleMockups() {
-    return new Promise(function(resolve, reject) {
+function styleMockups(onComplete) {
 
-        'use strict';
+    'use strict';
 
-        $('.container').css({
-            'margin-top': config.container.top,
-            height: config.container.height,
-            width: config.container.width
-        });
-        $('.container > .content').css({
-            height: config.content.height,
-            width: config.content.width,
-            left: config.content.x,
-            top: config.content.y
-        });
-
-        console.debug('style mockup containers');
-        return resolve();
+    $('.container').css({
+        'margin-top': config.container.top,
+        height: config.container.height,
+        width: config.container.width
     });
+    $('.container > .content').css({
+        height: config.content.height,
+        width: config.content.width,
+        left: config.content.x,
+        top: config.content.y
+    });
+
+    console.debug('style mockup containers');
+    if (onComplete) return onComplete();
 }
 
 /* ---------------------------------------------------------------------------
  *
  * @function loadMockupContent
  * Load mockup content from file
- * @return Promise resolved once all mockup content is loaded
  *
  */
 
-function loadMockupContent() {
-    return new Promise(function(resolve, reject) {
+function loadMockupContent(onComplete) {
 
-        'use strict';
+    'use strict';
 
-        var content = $('.container > .content'),
-            contentLen = content.length -1;
+    var content = $('.container > .content'),
+        contentLen = content.length -1;
 
-        $('.container > .content').each(function(i) {
-            if (this.dataset === {} ) return;
-            $(this).load(this.dataset.file +'.html', function() {
-                console.debug('loading content for '+ this.dataset.file);
-                if (contentLen === i) return resolve();
-            });
+    $('.container > .content').each(function(i) {
+        if (this.dataset === {} ) return;
+        $(this).load(this.dataset.file +'.html', function() {
+            console.debug('loading content for '+ this.dataset.file);
+            if (contentLen === i && onComplete) return onComplete();
         });
     });
 }
@@ -110,10 +101,13 @@ function loadMockupContent() {
 
         'use strict';
 
-        createMockups()
-        .then(function() { return styleMockups(); })
-        .then(function() { return loadMockupContent(); })
-        .then(function() { console.debug('complete'); });
+        createMockups(function() {
+            styleMockups(function() {
+                loadMockupContent(function() {
+                    console.debug('compelte');
+                });
+            });
+        });
     });
 
 }());
